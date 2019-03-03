96f82bf84843cfb92a2e445152ca47aecb2bc2c0
Diogo Cardoso <diogocardoso92@outlook.com>
2019-03-03 14:54:16 +0000

    Add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+const Container = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 2rem;
+  min-height: 60vh;
+`;
+
+const PageTitle = styled.h1`
+  text-align: center;
+  margin-bottom: 3rem;
+`;
+
+const Text = styled.p`
+  color: rgba(25, 25, 25, 0.9);
+  line-height: 1.5rem;
+`;
+
+const NotFoundPage = ({ location }) => (
+  <Layout location={location.pathname}>
+    <SEO title="404 Not Found" keywords={[`gatsby`, `application`, `react`]} />
+    <Container>
+      <PageTitle> 404 - Page not found </PageTitle>
+      <Text> Oooops, the page you are looking for does not exist. </Text>
+      <Link to="/">Back to Home</Link>
+    </Container>
   </Layout>
 );
 
