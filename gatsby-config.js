54e46d606a98f1179212a1d520a58f2b5340bc69
Diogo Cardoso <diogocardoso92@outlook.com>
2019-03-03 15:44:15 +0000

    Update website metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Diogo Cardoso`,
+    description: `Full-stack web developer based in Portugal. Masters in Electrical Engineering - Automation.`,
+    author: `Diogo Cardoso`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
