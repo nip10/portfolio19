40d771b659349950285e953886a3a28aa266164e
Diogo Cardoso <diogocardoso92@outlook.com>
2019-03-15 21:48:55 +0000

    Update about text

--- src/pages/about.js
+++ src/pages/about.js
@@ -17,8 +17,16 @@ const PageTitle = styled.h1`
 `;
 
 const Text = styled.p`
-  color: rgba(25, 25, 25, 0.9);
+  color: #090909;
   line-height: 1.5rem;
+`;
+
+const Link = styled.a`
+  text-decoration: none;
+  color: #a9982f;
+  :hover {
+    text-decoration: underline;
+  }
 `;
 
 const AboutPage = ({ location }) => (
@@ -26,17 +34,18 @@ const AboutPage = ({ location }) => (
     <SEO title="About" keywords={[`gatsby`, `application`, `react`]} />
     <Container>
       <PageTitle> About </PageTitle>
+      <Text>My name is Diogo and I'm a full-stack web-developer. Currently based in Porto, Portugal.</Text>
       <Text>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat modi facilis dolorum dolores incidunt. A saepe
-        cumque velit impedit aperiam distinctio, quo assumenda eaque unde, iure repellendus nemo, ullam amet.
+        I'm passionate about building web apps with clean, tested and maintainable code, using the latest technologies.
+        I have an aptitude for problem solving and a hunger for continuous improvement. I am looking to work as part of
+        a team where I can contribute to meaningful projects, while learning from my peers. I work primarily with a
+        JavaScript/TypeScript stack: NodeJS for back-end and React for front-end - but picking up new languages or
+        frameworks is not a problem. I'm currently in the final year of a Masters degree in Electrical & Computer
+        Engineering (Automation branch) at FEUP.
       </Text>
+      <Text>My 3 main interests are web-development, automation (IoT, Industrial) and project management.</Text>
       <Text>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat modi facilis dolorum dolores incidunt. A saepe
-        cumque velit impedit aperiam distinctio, quo assumenda eaque unde, iure repellendus nemo, ullam amet.
-      </Text>
-      <Text>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat modi facilis dolorum dolores incidunt. A saepe
-        cumque velit impedit aperiam distinctio, quo assumenda eaque unde, iure repellendus nemo, ullam amet.
+        My resumé is available <Link href="cv.pdf">here</Link>.
       </Text>
     </Container>
   </Layout>
