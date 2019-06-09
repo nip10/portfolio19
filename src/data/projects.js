c42f41c556b9783283d1ff3b77557a296909a8e0
Diogo Cardoso <diogocardoso92@outlook.com>
2019-06-09 18:48:43 +0100

    Add project gifs/imgs

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,18 +1,17 @@
+import euroImg from '../assets/projects/euro.gif';
+import todoImg from '../assets/projects/todo.gif';
+import shImg from '../assets/projects/short.gif';
+import chatImg from '../assets/projects/chat.gif';
+import portfolioImg from '../assets/projects/portfolio.gif';
+import apiImg from '../assets/projects/api.png';
+
 const projects = [
-  {
-    title: 'Blog',
-    description: 'A personal blog mostly about my areas of interest, namely: IoT, Automation, Web-Development.',
-    stack: ['Javascript', 'React', 'Gatsby'],
-    image: 'https://via.placeholder.com/500x300',
-    repo: 'https://github.com/nip10/blog',
-    demo: 'https://blog.diogocardoso.me',
-  },
   {
     title: 'Euromillions Notify',
     description:
       'Get an email notification when the euromillions prize is above a pre-defined threshold. Parses XML data from Santa Casa and runs a cronjob before the bi-weekly drafts. ',
     stack: ['Typescript', 'React', 'Styled Components', 'NodeJS', 'Express', 'MongoDB', 'Jest'],
-    image: 'https://via.placeholder.com/500x300',
+    image: euroImg,
     repo: 'https://github.com/nip10/todo-react-ts',
     demo: 'https://euronotify.diogocardoso.me',
   },
@@ -20,7 +19,7 @@ const projects = [
     title: 'URL Shortener',
     description: "Shorten url's with social sharing.",
     stack: ['Typescript', 'React', 'Styled Components', 'NodeJS', 'Express', 'MongoDB', 'Jest'],
-    image: 'https://via.placeholder.com/500x300',
+    image: shImg,
     repo: 'https://github.com/nip10/todo-react-ts',
     demo: 'https://sh.diogocardoso.me',
   },
@@ -28,7 +27,7 @@ const projects = [
     title: "Todo's Manager",
     description: "Front-end for managing todo's.",
     stack: ['Typescript', 'React', 'Styled Components'],
-    image: 'https://via.placeholder.com/500x300',
+    image: todoImg,
     repo: 'https://github.com/nip10/todo-react-ts',
     demo: 'https://todo.diogocardoso.me',
   },
@@ -36,7 +35,7 @@ const projects = [
     title: 'Todo api',
     description: "RESTful API for managing todo's.",
     stack: ['JavaScript', 'Typescript', 'NodeJS', 'Express', 'MongoDB', 'Jest'],
-    image: 'https://via.placeholder.com/500x300',
+    image: apiImg,
     repo: 'https://github.com/nip10/todo-api',
     demo: 'https://api.todo.diogocardoso.me',
   },
@@ -44,7 +43,7 @@ const projects = [
     title: 'Chat',
     description: 'Simple chat webapp with Geolocation API.',
     stack: ['HTML5', 'CSS3', 'JavaScript', 'NodeJS', 'Express', 'SocketIO'],
-    image: 'https://via.placeholder.com/500x300',
+    image: chatImg,
     repo: 'https://github.com/nip10/node-chat-app',
     demo: 'https://chat.diogocardoso.me',
   },
@@ -66,10 +65,18 @@ const projects = [
     repo: 'https://github.com/nip10/telegram-bot',
   },
   {
+    title: 'Blog',
+    description: 'A personal blog mostly about my areas of interest, namely: IoT, Automation, Web-Development.',
+    stack: ['Javascript', 'React', 'Gatsby'],
+    image: 'https://via.placeholder.com/500x300',
+    repo: 'https://github.com/nip10/blog',
+    demo: 'https://blog.diogocardoso.me',
+  },
+  {
     title: 'Portfolio v2',
     description: 'This website. Built with Gatsby.',
     stack: ['JavaScript', 'React', 'Styled Components', 'Gatsby'],
-    image: 'https://via.placeholder.com/500x300',
+    image: portfolioImg,
     repo: 'https://github.com/nip10/portfolio19',
     demo: 'https://diogocardoso.me',
   },
